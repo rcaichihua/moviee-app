e076f5ee2c5b11ac29f9f154d06b851c76275731
Roger Davy <rcaichihua@gmail.com>
2022-08-10 20:21:56 -0500

    Proyecto de prueba

---
+++ src/theme/index.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#43AA8B",
+      contrastText: "#fff",
+    },
+  },
+});
+
+export default theme;
